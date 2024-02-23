7b4dc11555a05c4745cc9e18b5a21fc9397bea59
zkhiraoui <za.khiraoui@gmail.com>
2024-02-23 03:33:38 -0800

    zkhiraoui - alx  devops

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import unittest
+from models.base_model import BaseModel
+
+class TestBaseModel(unittest.TestCase):
+    """Test suite for BaseModel class."""
+
+    def test_something(self):
+        """Test case for something."""
+        self.assertTrue(True)  # Example test case
+
+if __name__ == '__main__':
+    unittest.main()
